8da0b93be5133d0d107d566332d12da0aa2b5813
taviral55 <taviral55@gmail.com>
2023-11-14 20:58:58 -0800

    created the repository

---
+++ Develop/script.js
@@ -0,0 +1,33 @@
+// const password = document.getElementById("#password");
+// const lenght = 8;
+
+// const uppercase =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lowercase =  "abcdefghijklmnopqrstuvwxyz";
+// const number =  "0123456789";
+// const symbol =  "!@#$%^&*()";
+
+document.getElementById("#generate").on("click", function () {
+  alert("click")
+})
+
+
+
+
+
+// // Assignment code here
+
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
